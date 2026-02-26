099906f976f3b98f16096cf9c6ecd1285af2fdb1
Kauan <kauanlucas3003@gmail.com>
2026-02-26 14:27:25 -0300

    feat: setup express server with initial route

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.length("/", (req, res) => {
+    res.send("API funcionando");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
